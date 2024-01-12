bf84faebb08b9a0e429f0ddb151452a586a0d2ac
janghl <janghl2001@gmail.com>
2024-01-12 00:00:05 -0600

    add get->delete

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# New Node Application
+# Simple Node Application
+## To play around with get, post, put and delete:
+## Download Yet Another REST Client from google extension store
+## Code below gets information from item "sample":
+```
+http://localhost:5000/api/getMethod/sample
+```
+## Code below post item named "newName":
+```
+http://localhost:5000/api/postMethod/
+```
+## Payload:
+```
+{
+    "attr": "newName",
+    "age": "25"
+}
+```
+## Code below put item sample and change "age":
+```
+http://localhost:5000/api/putMethod/sample
+```
+## Payload:
+```
+{
+    "age": "23"
+}
+```
+## Code below delete item "sample":
+```
+http://localhost:5000/api/deleteMethod/sample
+```
--- dist/index.js
+++ dist/index.js
@@ -48,13 +48,45 @@ router.post("/postMethod/", (request,response) => {
     //create an item
     const newItem = {
         user: body.attr,
-        age: body.age,
+        age: age,
         name: "newlyCreated"
     };
 
     db[newItem.user] = newItem;
 
     return response.status(201).json(newItem)
+});
+router.put('/putMethod/:param', (request, response) => {
+    const body = request.body;
+    const param = request.params.param;
+    const content = db[param];
+    if(!content){
+        return response.status(404).json({error: "Wrong Name!"});
+    }
+    if(body.attr || body.name){
+        return response.status(400).json({error: "Only age is editable!"});
+    }
+    let age = body.age;
+    if(age && typeof(age)!=='number'){
+        age = parseFloat(age);
+        if(isNaN(age)){
+            return response.status(400).json({error: 'not a valid number!'});
+        }
+    }
+    if(body.age){
+        content.age = age;
+    }
+    return response.status(200).json(db)
+});
+router.delete('/deleteMethod/:toBeDeleted', (request, response) => {
+    const body = request.body;
+    const param = request.params.toBeDeleted;
+    content =  db[param];
+    if(!content){
+        return response.status(404).json({error: "cannot find the item!"});
+    }
+    delete db[param];
+    return response.status(204);
 });
 app.listen(port,() => {
     console.log("start a server!");
